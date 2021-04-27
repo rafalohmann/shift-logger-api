4d3a3b7b02e10b8095ff355dd027e196f9e44f50
Rafa Lohmann <raflohmann@gmail.com>
2021-04-27 05:59:07 -0300

    Add delete

--- src/controllers/log.ts
+++ src/controllers/log.ts
@@ -104,7 +104,7 @@ router.put(
     body('operator').notEmpty().isString().isLength({ max: 255 }),
     body('comment').notEmpty().isString().isLength({ max: 65535 }),
     wrap(async (req: Request, res: Response) => {
-        debug('[POST] /log', req.body);
+        debug('[PUT] /log/:id', req.body);
 
         const errors = validationResult(req);
         if (!errors.isEmpty()) {
@@ -125,6 +125,33 @@ router.put(
                 log.set('comment', comment);
                 const updatedlog = await log.save();
                 return res.json(updatedlog);
+            }
+            throw error(404, 'Not Found');
+        } catch (e) {
+            if (e.data) throw e;
+            throw error(500, 'Database Error');
+        }
+    }),
+);
+
+router.delete(
+    '/:id',
+    param('id').notEmpty().isInt({ min: 1 }),
+    wrap(async (req: Request, res: Response) => {
+        debug('[DELETE] /log/:id', req.body);
+
+        const errors = validationResult(req);
+        if (!errors.isEmpty()) {
+            return res.status(422).json({ errors: errors.array() });
+        }
+
+        const { id } = req.params;
+
+        try {
+            const log = await new Log().where({ id }).fetch();
+            if (log) {
+                await log.destroy();
+                return res.sendStatus(200);
             }
             throw error(404, 'Not Found');
         } catch (e) {
